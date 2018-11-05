b75c6c2be0dee8a2a3b658f59425b890f98657e3
DinoSaulo <sab2@cin.ufpe.br>
2018-11-05 17:16:16 -0300

    inserido slick

---
+++ Desafio 06/Site/javascript/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+  });
+// https://jsfiddle.net/solodev/dbowxqc6/
